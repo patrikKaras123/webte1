4ce8ee6de5946529a4eb8c9988d52e7d68b5c700
PeterKaras <64733540+PeterKaras@users.noreply.github.com>
2022-12-23 16:19:07 +0100

    Update DragDrop.tsx

--- src/components/DragDrop.tsx
+++ src/components/DragDrop.tsx
@@ -233,7 +233,7 @@ const DragDrop: FC<DragDropProps> = (props) => {
     const acceleration = Math.sqrt(x * x + y * y + z * z);
     //console.log(acceleration);
     // Adjust sensibility, because it can depend of usage (& devices)
-    const sensibility = 520;
+    const sensibility = 350;
     if (acceleration >= sensibility) {
       resetHandler();
     };
