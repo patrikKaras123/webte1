7dc2ccec4864ca4fbf97d050b40439383e05f65e
PeterKaras <64733540+PeterKaras@users.noreply.github.com>
2022-12-23 16:54:26 +0100

    Update constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -8,5 +8,5 @@ export const cards = ["../allFlags/slovakia.jpg", "../allFlags/micronesia.jpg", 
 export const description = ["White. Blue. Red. Coast-of-arms... Slovenia?", "What is this???", "Asian country as the rest. Still the same..."]
 export const headings = ["Slovakia", "Micronesia", "South Korea"]
 
-export const FLAG = "Drag the piece of flag on the right side. Put them all together underneath in the right way to advance to another level. On mobile, just shake and reset."
+export const FLAG = "Drag the piece of flag on the right side. Put them all together underneath in the right way to advance to another level. On mobile, just shake and reset. Please allow motion sensor in web settings."
 export const GAME = "Now. You should pick your difficulty level. There are 3 difficulty levels. Easy. Medium. Hard. The hard one is only for experts."
